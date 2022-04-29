a192a826a1b01f2fb9fa0083fff41ddfb09aa770
Liam <98435043+Filajabob@users.noreply.github.com>
2022-04-28 21:59:31 -0400

    Minor changes to battle.py

--- src/classes/battle.py
+++ src/classes/battle.py
@@ -39,39 +39,46 @@ class Battle:
         self.c_active = self.cdeck[c_active]
         self.d_active = self.ddeck[d_active]
 
-        if attacker == 'c':
-            self.attack = getattr(self.c_active, f"{attack}_attack")()
+        for i in range(0, 2):
+            if attacker == 'c':
+                self.attack = getattr(self.c_active, f"{attack}_attack")()
 
-            self.challenger.energy -= self.attack['energy-used']
-            self.d_active.hp -= self.attack['damage']
-        elif attacker == 'd':
-            self.attack = getattr(self.d_active, f"{attack}_attack")()
+                self.challenger.energy -= self.attack['energy-used']
+                self.d_active.hp -= self.attack['damage']
+            elif attacker == 'd':
+                self.attack = getattr(self.d_active, f"{attack}_attack")()
 
-            self.defender.energy -= self.attack['energy-used']
-            self.c_active.hp -= self.attack['damage']
-        else:
-            raise ValueError(f"Unknown attacker {attacker}, please refer to classes.Battle.rotate.__doc__")
+                self.defender.energy -= self.attack['energy-used']
+                self.c_active.hp -= self.attack['damage']
+            else:
+                raise ValueError(f"Unknown attacker {attacker}, please refer to classes.Battle.rotate.__doc__")
 
-        if self.d_active.hp <= 0:
-            self.defender.hp = None
-            del self.ddeck[d_active]
+            if self.d_active.hp <= 0:
+                self.defender.hp = None
+                del self.ddeck[d_active]
 
-        if self.c_active.hp <= 0:
-            self.challenger.hp = None
-            del self.cdeck[c_active]
+            if self.c_active.hp <= 0:
+                self.challenger.hp = None
+                del self.cdeck[c_active]
 
-        # Too lazy to make it so you can't "go into debt" for energy, maybe will add later
-        if (self.challenger.energy <= 0 or len(self.cdeck) == 0) and not (
-                self.defender.energy <= 0 or len(self.ddeck) == 0):
-            self.winner = "defender"
+            # Too lazy to make it so you can't "go into debt" for energy, maybe will add later
+            if (self.challenger.energy <= 0 or len(self.cdeck) == 0) and not (
+                    self.defender.energy <= 0 or len(self.ddeck) == 0):
+                self.winner = "defender"
 
-        elif not (self.challenger.energy <= 0 or len(self.cdeck) == 0) and (
-                self.defender.energy <= 0 or len(self.ddeck) == 0):
-            self.winner = "challenger"
+            elif not (self.challenger.energy <= 0 or len(self.cdeck) == 0) and (
+                    self.defender.energy <= 0 or len(self.ddeck) == 0):
+                self.winner = "challenger"
 
-        elif (self.challenger.energy <= 0 or len(self.cdeck) == 0) and (
-                self.defender.energy <= 0 or len(self.cdeck) == 0):
-            self.winner = "tie"
+            elif (self.challenger.energy <= 0 or len(self.cdeck) == 0) and (
+                    self.defender.energy <= 0 or len(self.cdeck) == 0):
+                self.winner = "tie"
+
+            # Make sure the other side gets a turn
+            if attacker == 'c':
+                attacker = 'd'
+            else:
+                attacker = 'c'
 
         self.rotation_num += 1
 
