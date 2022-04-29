be5bb6c1391ad207619790a877b25b68b79cdea2
Liam <98435043+Filajabob@users.noreply.github.com>
2022-04-28 21:36:05 -0400

    Almost finished battle functionality

--- src/classes/abc.py
+++ src/classes/abc.py
@@ -1,15 +1,16 @@
 class Card:
-    def __init__(self, xp=None, hp=None, att_score=None):
+    def __init__(self, xp=None, hp=None, att_score=None, name="Generic Card"):
         """
         To load a card from a JSON file, use Python's **kwargs utility.
 
-        :param xp:
-        :param hp:
-        :param att_score:
+        :param xp: Shorthand for experience
+        :param hp: Shorthand for hitpoints. Hitpoints is the amount of damage you can take until "dying"
+        :param att_score: A score that is relative to how many attacks the card has done.
         """
         self.xp = xp
         self.hp = hp
         self.att_score = att_score
+        self.name = name
 
         if not xp is None:
             self.level = xp // 100
@@ -44,10 +45,11 @@ class Card:
     def serialize(self):
         """
         Convert a card's data to a JSON compatible format.
-        :return:
+        :return: dict
         """
 
         return {
+            "name": self.name,
             "xp": self.xp,
             "hp": self.hp,
             "att_score": self.att_score,
--- src/classes/battle.py
+++ src/classes/battle.py
@@ -1,6 +1,8 @@
+from .cards import Card
+
+
 class Battle:
     def __init__(self, challenger, defender, cdeck, ddeck):
-
         if not len(cdeck) <= 3 or not len(ddeck) <= 3:
             raise ValueError("Active deck cannot contain more than 3 cards.")
 
@@ -13,11 +15,79 @@ class Battle:
         self.challenger = challenger
         self.defender = defender
 
+        self.winner = None
+
         self.rotation_num = 1
 
-    def rotate(self, c_active, d_active, attacker):
+        self.attack = None
+
+    def rotate(self, c_active: int, d_active: int, attacker: str, attack: str):
+        """
+        Rotate to the next rotation.
+
+        :param c_active: The active card index of the challenger.
+        :param d_active: The active card index of the defender.
+        :param attacker: A char representing which person is attacking. For challenger, input 'c', for defender, input
+        'd'.
+        :param attack: The attack done by the attacker. For primary attack, input "primary", etc.
+        :return:
+        """
+
+        if self.winner:
+            raise TypeError("Battle has already ended")
+
         self.c_active = self.cdeck[c_active]
         self.d_active = self.ddeck[d_active]
 
+        if attacker == 'c':
+            self.attack = getattr(self.c_active, f"{attack}_attack")()
 
+            self.challenger.energy -= self.attack['energy-used']
+            self.d_active.hp -= self.attack['damage']
+        elif attacker == 'd':
+            self.attack = getattr(self.d_active, f"{attack}_attack")()
 
+            self.defender.energy -= self.attack['energy-used']
+            self.c_active.hp -= self.attack['damage']
+        else:
+            raise ValueError(f"Unknown attacker {attacker}, please refer to classes.Battle.rotate.__doc__")
+
+        if self.d_active.hp <= 0:
+            self.defender.hp = None
+            del self.ddeck[d_active]
+
+        if self.c_active.hp <= 0:
+            self.challenger.hp = None
+            del self.cdeck[c_active]
+
+        # Too lazy to make it so you can't "go into debt" for energy, maybe will add later
+        if (self.challenger.energy <= 0 or len(self.cdeck) == 0) and not (
+                self.defender.energy <= 0 or len(self.ddeck) == 0):
+            self.winner = "defender"
+
+        elif not (self.challenger.energy <= 0 or len(self.cdeck) == 0) and (
+                self.defender.energy <= 0 or len(self.ddeck) == 0):
+            self.winner = "challenger"
+
+        elif (self.challenger.energy <= 0 or len(self.cdeck) == 0) and (
+                self.defender.energy <= 0 or len(self.cdeck) == 0):
+            self.winner = "tie"
+
+        self.rotation_num += 1
+
+        return {
+            "attack": self.attack,
+            "winner": self.winner,
+            "energy": {
+                "challenger": self.challenger.energy,
+                "defender": self.defender.energy
+            },
+            "hp": {
+                "challenger": self.c_active.hp,
+                "defender": self.d_active.hp
+            },
+            "decks": {
+                "challenger": self.cdeck,
+                "defender": self.ddeck
+            }
+        }
