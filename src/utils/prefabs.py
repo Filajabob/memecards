7198a6fdfc508e3971e8c70eacdcc47bfbb129e9
Liam <98435043+Filajabob@users.noreply.github.com>
2022-05-02 21:54:59 -0400

    idk

--- src/utils/prefabs.py
+++ src/utils/prefabs.py
@@ -13,13 +13,24 @@ def load_user(self, did, path=Constants.USERS_JSON):
 
 def stats(did):
     did = str(did)
-    user = load_user(Constants.USERS_JSON)
+    user = load_user(Constants.USERS_JSON, did)
     em = discord.Embed(title="Stats")
 
     # Start with win/loss stats
     em.add_field(name="Wins", value=user.stats["wins"])
     em.add_field(name="Losses", value=user.stats["losses"])
     em.add_field(name="Ties", value=user.stats["ties"])
-    em.add_field(name="Win/Loss Ratio", value=user.stats["wins"] / user.stats["losses"])
+
+    if user.stats["losses"] != 0:
+        em.add_field(name="Win/Loss Ratio", value=user.stats["wins"] / user.stats["losses"])
+    else:
+        em.add_field(name="Win/Loss Ratio", value="0")
 
     return em
+
+
+def user_exists(did):
+    with open(Constants.USERS_JSON, 'r') as f:
+        data = json.load(f)
+
+    return str(did) in data
